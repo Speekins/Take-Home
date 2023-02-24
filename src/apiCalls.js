bfb89c2aea6c410bd412639410916792f6bd9d76
Spencer Haka <spencer.haka@gmail.com>
2023-02-24 09:33:15 -0700

    fix integration issue

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,4 @@
-const MY_KEY = process.env.REACT_APP_API_KEY
+//const MY_KEY = process.env.REACT_APP_API_KEY
 
 export const getArticlesByGenre = (genre) => {
   return fetch(`https://api.nytimes.com/svc/topstories/v2/${genre}.json?api-key=OVAGeCoI4G285YyTXNDwGpYOsV0H55eA`)
